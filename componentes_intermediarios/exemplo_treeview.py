9e5642d398d1b69eeb9be44132c703a5fd0773f6
Daniel <danielbinsemp@gmail.com>
2025-08-08 10:44:34 -0500

    aula de uml

--- componentes_intermediarios/exemplo_treeview.py
+++ componentes_intermediarios/exemplo_treeview.py
@@ -4,6 +4,8 @@ from tkinter import ttk
 class Tela:
     def  __init__(self, master):
         self.janela = master
+
+
         colunas = ['nome', 'cpf', 'email']
         self.tvw = ttk.Treeview(self.janela, height=5, columns=colunas, show='headings')
         #Configurar o cabeça
@@ -23,8 +25,20 @@ class Tela:
         self.brl.grid(row=0, column=1, sticky='ns')
         self.tvw.configure(yscrollcommand=self.brl.set)
         self.tvw.grid(row=0, column=0)
+        self.centralizar(self.janela)
 
+    def centralizar(self, master):
+        # Criando uma forma centralizar a janela
+        largura_monitor = self.janela.winfo_screenwidth()
+        altura_monitor = self.janela.winfo_screenheight()
+        master.update_idletasks()
 
+        largura_janela= master.winfo_width()
+        altura_janela= master.winfo_height()
+        x = largura_monitor // 2 - largura_janela //2
+        y = altura_monitor //2 - altura_janela//2
+        master.geometry(f'{largura_janela}x{altura_janela}+{x}+{y}')
+        print(largura_janela)
 
 gui = tk.Tk()
 Tela(gui)
